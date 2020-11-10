9f26120b98319af35d33b08a64ba1cc4c955245b
BarryLiu <3273376560@qq.com>
2020-11-10 18:01:45 +0800

    get some specific information from the subject's data

---
+++ gui/get_info.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+'''
+@File : get_info.py
+@Author : BarryLiu
+@Time : 2020/11/10 16:28
+@Desc : get some specific information from the subject's data
+'''
+
+# To get GWR re-reference, we need to know the anatomical information
+# of each electrode, here are the functions we need.
+
+#
+
+def get_mni_struct():
+
+    pass
+
+
+
+def load_seeg_loc():
+
+    pass
+
+
+
+def get_anat_loc():
+
+    pass
+
+
+def get_brain_coord():
+
+    pass
+
+
+def stat_struct():
+
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
