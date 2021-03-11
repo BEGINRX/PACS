a3bd7ac0bfd1cd09ffb02e823877c590cb80f80c
刘政(Barry Liu) <65115609+BarryLiu-97@users.noreply.github.com>
2021-03-11 19:51:52 +0800

    solve theading queue problem in ubuntu system

--- main.py
+++ main.py
@@ -8,11 +8,25 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import Qt, QCoreApplication
 from gui.main_window import MainWindow
 from gui.splash_screen import SplashPanel
 
 
 def main():
+    '''
+    该代码需在创建QApplication实例之前
+    解决在ubuntu系统下出现的报错：具体如下：
+    [xcb] Unknown request in queue while appending request [xcb] Most likely this is a multi-threaded client and XInitThreads has not been called [xcb]
+    Aborting, sorry about that.
+    python: ../../src/xcb_io.c:151:
+    append_pending_request: Assertion `!xcb_xlib_unknown_req_pending'
+    failed
+    参考自https://forum.qt.io/topic/85774/qt-application-crash/6
+    '''
+
+    QCoreApplication.setAttribute(Qt.AA_X11InitThreads)
+
     my_app = QApplication(sys.argv)
     splash = SplashPanel()
     my_app.processEvents()
