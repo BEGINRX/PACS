d0c5eaf9463683751f63b2c2a90d5eccd2cd1d47
Barry <3273376560@qq.com>
2021-08-18 14:55:41 +0800

    update get_group_chan and bipolar functions

--- gui/re_ref.py
+++ gui/re_ref.py
@@ -87,162 +87,60 @@ def preprocess(seeg, path, name, sfreq=1000, low=0.5, high=200,
     seeg.save(os.path.join(path, name + '.fif'))
 
     return seeg
-
-
-
-def get_chan_group_old(raw):
-    '''
-    :param raw: instance of Raw
-                raw data
-    :return: dict
-             electrodes in the same shaft
-    '''
-    ch_names = raw.ch_names
-    ch_group = []
-    for ch_name in ch_names:
-        ch_group.append(ch_name[:2])
-    ch_group = list(set(ch_group))
-    num = [str(i) for i in range(10)]
-    for index in range(len(ch_group)):
-        if len(ch_group[index]) == 2:
-            if ch_group[index][1] in num:
-                ch_group[index] = ch_group[index][0]
-    ch_group = sorted(list(set(ch_group)))
-    # 获取每个轴上的电极名称，但如A'1的仍在A中
-    ch_group_cont = {group: [] for group in ch_group}
-    for group in ch_group:
-        for index in range(len(ch_names)):
-            if group in ch_names[index]:
-                ch_group_cont[group].append(ch_names[index])
-    # 将错分的电极删掉 如 A'1 在轴A中
-    length = [len(ch_group_cont[i]) for i in ch_group_cont]
-    i = 0
-    chan_del = {group: [] for group in ch_group_cont}
-    for group in ch_group_cont:
-        for index in range(length[i]):
-            if (group != 'DC') and ('DC' in ch_group_cont[group][index]):
-                chan_del[group].append(ch_group_cont[group][index])
-            if ('\'' not in group) and ((group + '\'') in ch_group_cont[group][index]):
-                chan_del[group].append(ch_group_cont[group][index])
-        i += 1
-
-    try:
-        del ch_group_cont['DC']
-    except Exception as error:
-        if error.args[0] == "KeyError: 'E'":
-            pass
-    try:
-        for group in ch_group_cont:
-            [ch_group_cont[group].remove(chan) for chan in chan_del[group]]
-            if '\'' in group:
-                tip = None
-                for index in range(len(ch_group_cont[group])):
-                    if ch_group_cont[group][index] == group:
-                        tip = [index, group]
-                    break
-                if tip:
-                    ch_group_cont[group].remove(tip[1])
-                    ch_group_cont[group].sort(key=lambda chan: (chan[0], int(chan[2:])))
-                    ch_group_cont[group].insert(0, tip[1])
-                else:
-                    ch_group_cont[group].sort(key=lambda chan: (chan[0], int(chan[2:])))
-            else:
-                tip = None
-                for index in range(len(ch_group_cont[group])):
-                    if ch_group_cont[group][index] == group:
-                        tip = [index, group]
-                    break
-                if tip:
-                    ch_group_cont[group].remove(tip[1])
-                    ch_group_cont[group].sort(key=lambda chan: (chan[0], int(chan[1:])))
-                    ch_group_cont[group].insert(0, tip[1])
-                else:
-                    ch_group_cont[group].sort(key=lambda chan: (chan[0], int(chan[1:])))
-    except:
-        traceback.print_exc()
-
-    return ch_group_cont
-
+    
+    
+    
 def get_chan_group(raw=None, chans=None):
     '''
+    This code could deal with multiple situations like the lenght of a group is 2
+    Note that the channels' name should be standard
     :param raw: instance of Raw
                 raw data
            chans: list of channels' name
     :return: dict
              electrodes in the same shaft
     '''
+    import re
+    import pandas as pd
     if raw is not None and chans is None:
         print(raw)
         chans = raw.ch_names
-        try:
-            raw.rename_channels({chan: chan[4:] for chan in raw.ch_names
-                                 if 'POL' in chan})
-            raw.rename_channels({chan: chan[4:6] for chan in raw.ch_names
-                                 if 'Ref' in chan})
-            useless_chan = [chan for chan in raw.ch_names if 'DC' in chan or 'BP' in chan
-                            or 'EKG' in chan or 'EMG' in chan]
-            raw.drop_channels(useless_chan)
-        except:
-            traceback.print_exc()
-    else:
+    try:
+        chans.pop(chans.index('E'))
+    except:
         pass
-    
-    if '-' not in chans[0]:
-        key = list(set([ch[0] for ch in chans]))
-        key += [k + '\'' for k in key]
-        chan_group = {k: [] for k in key}
-        for ch in chans:
-            for k in key:
-                if ch.startswith(k):
-                    if not k.endswith('\'') and ('\'' in ch):
-                        continue
-                    else:
-                        chan_group[k].append(ch)
-    
-        # delete no-element group(s)
-        chan_del = []
-        for group in chan_group:
-            if not len(chan_group[group]):
-                chan_del.append(group)
-        [chan_group.pop(ch) for ch in chan_del]
-    
-        # sort the channels in its group for reference
-        for group in chan_group:
+
+    chan_df = pd.DataFrame(columns=['Channel', 'Group'])
+    chan_df['Channel'] = chans
+    for chan in chans:
+        num_start = re.search(f'\d', chan).start()
+        chan_df.loc[chan_df['Channel'] == chan, ['Group']] = chan[:num_start]
+    group = list(set(chan_df['Group']))
+    group.sort()
+    chan_group = {gname: [] for gname in group}
+    for chan in chans:
+        [chan_group[gname].append(chan) for gname in group if
+         chan_df.loc[chan_df['Channel'] == chan]['Group'].item() == gname]
+    # sort the channels in its group for reference, just in case
+    for group in chan_group:
+        if '-' not in chans[0]:
             if group.endswith('\''):
-                if group in chan_group[group]:
-                    chan_group[group].remove(group)
-                    chan_group[group].sort(key=lambda ch: (ch[0], int(ch[2:])))
-                    chan_group[group].insert(0, group)
-                else:
-                    chan_group[group].sort(key=lambda ch: (ch[0], int(ch[2:])))
+                chan_group[group].sort(key=lambda ch: (ch[0:len(group)],
+                                                       int(ch[len(group):])))
             else:
-                if group in chan_group[group]:
-                    chan_group[group].remove(group)
-                    chan_group[group].sort(key=lambda ch: (ch[0], int(ch[1:])))
-                    chan_group[group].insert(0, group)
-                else:
-                    chan_group[group].sort(key=lambda ch: (ch[0], int(ch[1:])))
-    else:
-        # 先假设所有电极都有A和A’两种形式 再删除没有电极的group
-        key = list(set([ch[0] for ch in chans]))
-        key += [k + '\'' for k in key]
-        chan_group = {k: [] for k in key}
-        for ch in chans:
-            for k in key:
-                if ch.startswith(k):
-                    if not k.endswith('\'') and ('\'' in ch):
-                        continue
-                    else:
-                        chan_group[k].append(ch)
-    
-        # delete no-element group(s)
-        chan_del = []
-        for group in chan_group:
-            if not len(chan_group[group]):
-                chan_del.append(group)
-        [chan_group.pop(ch) for ch in chan_del]
+                chan_group[group].sort(key=lambda ch: (ch[0:len(group)],
+                                                       int(ch[len(group):])))
+        else:
+            if group.endswith('\''):
+                chan_group[group].sort(key=lambda ch: (ch[0:len(group)],
+                                                       int(ch[len(group):ch.index('-')])))
+            else:
+                chan_group[group].sort(key=lambda ch: (ch[0:len(group)],
+                                                       int(ch[len(group):ch.index('-')])))
 
     return chan_group
+
+
 
 
 def car_ref(raw, data_class='raw'):
@@ -318,53 +216,48 @@ def bipolar_ref(ieeg):
     from mne.io import BaseRaw
     from mne import BaseEpochs
     group_chan = get_chan_group(ieeg)
-    group_ieeg = {group: ieeg.copy().pick_channels(group_chan[group]).reorder_channels(group_chan[group])
-                  for group in group_chan}
-# =============================================================================
-#     if data_class == 'raw':
-#         group_ieeg_bipolar = {group: np.diff(group_ieeg[group]._data, axis=0) for group in group_chan}
-#     elif data_class =='epoch':
-#         group_ieeg_bipolar = {group: np.diff(group_ieeg[group]._data, axis=1) for group in group_chan}
-# =============================================================================
+    ieeg.load_data()
+    group_ieeg = {
+        group: ieeg.copy().pick_channels(group_chan[group]).reorder_channels(group_chan[group])
+        for group in group_chan}
     group_ieeg_bipolar = dict()
     if isinstance(ieeg, BaseRaw):
-        group_ieeg_bipolar = {group: group_ieeg[group]._data[:-1, :] - 
-                              group_ieeg[group]._data[1:, :]
+        group_ieeg_bipolar = {group: group_ieeg[group]._data[:-1, :] -
+                                     group_ieeg[group]._data[1:, :]
                               for group in group_chan}
     elif isinstance(ieeg, BaseEpochs):
-        group_ieeg_bipolar = {group: np.diff(group_ieeg[group]._data, axis=1) for group in group_chan}
+        group_ieeg_bipolar = {group: np.diff(group_ieeg[group]._data, axis=1) for group in
+                              group_chan}
 
     for group in group_ieeg:
         ch_name = group_ieeg[group].ch_names
         group_ieeg[group].drop_channels(group_chan[group][-1])._data = group_ieeg_bipolar[group]
 
-    
     group_0 = list(group_chan.keys())[0]
     bipolar_ieeg = group_ieeg[group_0]
     group_ieeg.pop(group_0)
     for name in group_ieeg:
         if not (name == 'DC'):
             bipolar_ieeg.add_channels([group_ieeg[name]])
-            
+    del ieeg
+    del group_ieeg
+
     bipolar_chan = bipolar_ieeg.ch_names
     bipolar_group = get_chan_group(chans=bipolar_chan)
-    
+
     for key in bipolar_group:
-            b_group = bipolar_group[key]
-            for index in range(len(b_group)):
-                chan = b_group[index]
-                if index == len(b_group) - 1:
-                    if '\'' not in chan:
-                        bipolar_name = chan + '-' + chan[0:1] + str(int(chan[1:]) + 1)
-                    else:
-                        bipolar_name = chan + '-' + chan[0:2] + str(int(chan[2:]) + 1)
-                    bipolar_ieeg.rename_channels({chan: bipolar_name})
-                else:
-                    latter_chan = b_group[index + 1]
-                    bipolar_name = chan + '-' + latter_chan
-                    bipolar_ieeg.rename_channels({chan: bipolar_name})
-                print('Successfully convert {:} to {:}'.format(chan, bipolar_name))
-    
+        b_group = bipolar_group[key]
+        for index in range(len(b_group)):
+            chan = b_group[index]
+            if index == len(b_group) - 1:
+                bipolar_name = chan + '-' + chan[0:len(key)] + str(int(chan[len(key):]) + 1)
+                bipolar_ieeg.rename_channels({chan: bipolar_name})
+            else:
+                latter_chan = b_group[index + 1]
+                bipolar_name = chan + '-' + latter_chan
+                bipolar_ieeg.rename_channels({chan: bipolar_name})
+            print('Successfully convert {:} to {:}'.format(chan, bipolar_name))
+
     return bipolar_ieeg
 
 
